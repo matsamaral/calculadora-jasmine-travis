6d94dfb54c670f184b51f7619a794cfd484eeebe
Mateus Augusto Amaral <mamaral@viceri.com.br>
2020-05-26 00:40:31 -0300

    Refatoração do código da calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,25 +1,52 @@
 var Calculadora = {
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISAO: '/',
+    MULTIPLICACAO: '*',
     adicionar: function (num1, num2) {
-
-        if(isNaN(num1) || isNaN(num2)) { return 0 };
-        num1 = parseFloat(num1);
-        num2 = parseFloat(num2);
-
-        return num1 + num2;
+        return this.calcular(num1, num2, this.ADICAO);
     },
     subtrair: function (num1, num2) {
-        if(isNaN(num1) || isNaN(num2)) { return 0 };
-        
-        return num1 - num2;
+        return this.calcular(num1, num2, this.SUBTRACAO);
     },
     dividir: function (num1, num2) {
-        if(num2 === 0) {return 'Erro'};
-        if(isNaN(num1) || isNaN(num2)) { return 0 };
-        return num1 / num2;
+        if (num2 === 0) { return 'Erro'};
+        return this.calcular(num1, num2, this.DIVISAO);
     },
     multiplicar: function (num1, num2) {
-        if(isNaN(num1) || isNaN(num2)) { return 0 };
-        return num1 * num2;
+        return this.calcular(num1, num2, this.MULTIPLICACAO);
+    },
+
+    calcular: function(num1, num2, operacao) {
+        var resultado = 0
+
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        
+        if(isNaN(num1) || isNaN(num2)) { 
+            return 0 
+        };
+
+        switch(operacao) {
+            case Calculadora.ADICAO:
+                resultado = num1 + num2;
+                break;
+                
+            case Calculadora.SUBTRACAO:
+                resultado = num1 - num2;
+                break;
+            case Calculadora.DIVISAO:
+                resultado = num1 / num2;
+                break;
+            case Calculadora.MULTIPLICACAO:
+                resultado = num1 * num2;
+                break;
+            default:
+                resultado = 0;
+                break;
+        }
+
+        return resultado;
     }
 };
 
